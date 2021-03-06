6f09ff4dd57fc7dc3222c03945b45aebcbd49f67
lancerdonnie <jideadedejifirst@gmail.com>
2021-03-06 12:39:45 +0100

    trying raww query

--- backend/src/createConn.ts
+++ backend/src/createConn.ts
@@ -1,4 +1,5 @@
 import { getConnection } from 'typeorm';
+import { Club } from './entity/Club';
 
 const { createConnection } = require('typeorm');
 
@@ -6,6 +7,9 @@ const createConn = async () => {
   try {
     await createConnection();
     console.log('Database Connected');
+    // const c = await Club.query(`
+    // SELECT * FROM club c INNER JOIN club_members cm ON cm.clubId = c.id`);
+    // console.log(c);
   } catch (error) {
     console.error(error);
     throw error;
